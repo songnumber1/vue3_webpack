a5f2b06ddf0a50a197358d4b9921e141933cc3d6
song <songnumber1@naver.com>
2023-12-20 21:35:37 +0900

    vue3, vuetify 3 설치 완료

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
+  chainWebpack: config => {
+      config.module.rules.delete('eslint');
+  },
+
   transpileDependencies: true,
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
 });
