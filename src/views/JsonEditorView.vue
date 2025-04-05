cadeeb6567a940b3c09c049f27913758e6c20219
song <songnumber1@naver.com>
2025-04-05 11:46:59 +0900

    미리보기 추가

--- src/views/JsonEditorView.vue
+++ src/views/JsonEditorView.vue
@@ -1,11 +1,18 @@
 <template>
   <div style="padding: 10px">
     <h3>Redgoose JSON Editor</h3>
-    <div
-      class="editor-wrapper editor-scroll-box"
-      ref="editorContainer"
-      style="height: 420px"
-    ></div>
+    <div style="display: flex">
+      <div
+        class="editor-wrapper editor-scroll-box"
+        ref="editorContainer"
+        style="height: 420px; width: 50%"
+      ></div>
+      <textarea
+        style="height: 420px; width: 50%; border: 1px solid"
+        v-model="jsonData"
+      >
+      </textarea>
+    </div>
     <button @click="printData">현재 데이터 콘솔 출력</button> <br />
     <button @click="exportData">export</button> <br />
     <button @click="expandData">expand</button> <br />
@@ -26,60 +33,44 @@ export default {
       editor: null,
       wrap: null,
       handleContextEvent: null,
+      handleUpdateEvent: null,
+      previewData: null,
       jsonData: [
-        [
-          {
-            userId: 1,
-            id: 1,
-            title:
-              "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-          },
-          {
-            userId: 1,
-            id: 2,
-            title: "qui est esse",
-            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-          },
-          {
-            userId: 1,
-            id: 3,
-            title:
-              "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-            body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
-          },
-        ],
-        [
-          {
-            userId: 1,
-            id: 4,
-            title: "eum et est occaecati",
-            body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
-          },
-          {
-            userId: 1,
-            id: 5,
-            title: "nesciunt quas odio",
-            body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
-          },
-          {
-            userId: 1,
-            id: 6,
-            title: "dolorem eum magni eos aperiam quia",
-            body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae",
-          },
-          {
-            userId: 1,
-            id: 7,
-            title: "magnam facilis autem",
-            body: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
-          },
-        ],
+        {
+          userId: 1,
+          id: 1,
+          title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "qui est esse",
+          body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+          body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+        },
       ],
     };
   },
+  computed: {
+    editJson() {
+      if (this.editor) {
+        return JSON.stringify(this.editor.node, null, 2);
+      } else {
+        return null;
+      }
+    },
+  },
   mounted() {
     this.initEditor();
+
+    this.jsonData = JSON.stringify(this.jsonData, null, 2);
   },
   beforeUnmount() {
     this.destroyEditor();
@@ -110,6 +101,10 @@ export default {
       };
 
       this.wrap.addEventListener("context", this.handleContextEvent);
+
+      this.wrap.addEventListener("update", ({detail}) => {
+        this.onEditorUpdate(detail);
+      });
     },
 
     destroyEditor() {
@@ -121,6 +116,10 @@ export default {
         this.wrap.removeEventListener("context", this.handleContextEvent);
         this.handleContextEvent = null;
       }
+
+      this.wrap.removeEventListener("update", ({detail}) => {
+        this.onEditorUpdate(detail);
+      });
     },
     printData() {
       try {
@@ -150,8 +149,8 @@ export default {
       this.initEditor();
     },
     onEditorUpdate(data) {
-      console.log("updated data", data, JSON.stringify(data));
-      this.jsonData = data;
+      console.log("updated data", JSON.stringify(data));
+      this.jsonData = JSON.stringify(data, null, 2);
     },
   },
 };
