34f27a36f38e73a041b440535527a5ebdad39d16
song <songnumber1@naver.com>
2025-06-27 23:34:08 +0900

    markdown-it-mermaid 설명 추가

--- src/components/MathMarkdown.vue
+++ src/components/MathMarkdown.vue
@@ -16,8 +16,11 @@ import katex from "katex";
 import "katex/dist/katex.min.css";
 import {renderMathInElement} from "mathup";
 import mermaid from "mermaid";
+// eslint-disable-next-line no-unused-vars
+import markdownItMermaid from "markdown-it-mermaid";
 
 // Mermaid 코드 블럭 wrapper
+// eslint-disable-next-line no-unused-vars
 function mermaidCodeBlockWrapper(md) {
   const defaultFence =
     md.renderer.rules.fence ||
@@ -58,6 +61,11 @@ export default {
           },
         },
         {plugin: mermaidCodeBlockWrapper},
+        // markdownItMermaid 모두 정상적으로 렌더링하지 못함ㅁ
+        // 위의 mermaidCodeBlockWrapper하고 같이 mermaid를 렌더링하므로 //
+        // mermaidCodeBlockWrapper보다 늦게 선언된 markdownItMermaid 기준으로 렌더링하므로 아래 주석 코드를 해제하면
+        // 정상적으로 렌더링이 안된다
+        // {plugin: markdownItMermaid},
       ];
     },
   },
