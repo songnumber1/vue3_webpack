5938e63a76bc5bcddb4c68af31f567e049f50535
song <songnumber1@naver.com>
2025-07-04 23:26:24 +0900

    vue 버전 3.5.17으로 업그레이드

--- src/AppVuetify.vue
+++ src/AppVuetify.vue
@@ -21,16 +21,13 @@ body {
   height: 100%;
   margin: 0;
   padding: 0;
-  overflow: hidden; /* ✅ 핵심 */
 }
 
 #app {
   height: 100%;
-  overflow: hidden; /* ✅ 이것도 중요 */
 }
 
 .v-application {
   height: 100%;
-  overflow: hidden;
 }
 </style>
